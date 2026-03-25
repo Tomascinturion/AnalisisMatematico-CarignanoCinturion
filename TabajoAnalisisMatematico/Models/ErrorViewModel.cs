b1ca7f725ef69383496eba457eb8fb3cabb2ffe0
Tomascinturion <tomascinturionices@gmail.com>
2026-03-25 18:55:30 -0300

    Sistema

---
+++ TabajoAnalisisMatematico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TabajoAnalisisMatematico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
